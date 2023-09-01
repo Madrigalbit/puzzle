3b586e62769e30cdfb8fd1093c778d816d4ba9ad
Matthew Madrigal <46862219+Madrigalbit@users.noreply.github.com>
2023-09-01 10:37:57 -0700

    Update and rename commandpromptv46.js to commandpromptv47.js

--- commandpromptv47.js
+++ commandpromptv47.js
@@ -35,15 +35,12 @@ function handleCommand(command) {
     // Sample commands and responses
     switch (command.toLowerCase()) {
         case 'help':
-            appendOutput('\nWelcome to the Galactic Prison Terminal Interface! To navigate through the system and access various programs, please use the following commands:\n\n● LIST.PROGS: Displays a list of available programs and their descriptions.\n● INFO [program_name]: Provides detailed information about the selected program.\n● LOGOUT: Logs you out of the terminal and returns you to the main menu.\nTo get started, simply type your desired command and follow the prompts. Keep your limbs and tentacles inside the terminal at all times, and dont access any programs that you dont have clearance to.\n');
+            appendOutput('\nWelcome to the Galactic Prison Terminal Interface! To navigate through the system and access various programs, please use the following commands:\n\n<br></br>● LIST.PROGS: Displays a list of available programs and their descriptions.\n● INFO [program_name]: Provides detailed information about the selected program.\n● LOGOUT: Logs you out of the terminal and returns you to the main menu.\nTo get started, simply type your desired command and follow the prompts. Keep your limbs and tentacles inside the terminal at all times, and dont access any programs that you dont have clearance to.\n');
             break;
         case 'info':
             appendOutput('Display list of staff names at current installation. Example: “staff/gumbert”\nGumbert\nClippy\nJanitor\nAdmin\n');
             break;
-        case 'help':
-            const helpMessage = "Available commands:\n- hello: Displays a greeting.\n- date: Displays the current date.";
-            appendOutput(helpMessage);
-            break;
+
         default:
             appendOutput(`Command not recognized: ${command}`);
             break;
