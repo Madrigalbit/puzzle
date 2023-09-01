fb984bb8269c3b90aebce65548683c403c8685b2
Matthew Madrigal <46862219+Madrigalbit@users.noreply.github.com>
2023-09-01 02:17:30 -0700

    Update and rename commandpromptv10.js to commandpromptv11.js

--- commandpromptv11.js
+++ commandpromptv11.js
@@ -1,8 +1,8 @@
-        document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
             const input = document.getElementById("command-input");
             const output = document.querySelector(".output");
 
-            output.innerHTML = "PE-T1 Data Terminal Osiris.<br />Stardate 2552.<br />ROM BIOS LOADED v9.2 - rev 7<br />RAM CRC check: (000000h)<br />Drive - Serial: 4204694 - DETECTED<br />INPUT SYSTEM - VIRTUAL<br />Booting.<br />Ports SECURE: [TAPS: 00]<br />Encryption:[ENABLED-204umn1]<br />Virus Scan - '.....CHECKING…..'<br />BOOT: ProspectEdenAdmin.exe<br />BOOT SUCCESSFUL.<br />WELCOME.<br />Welcome USER to Prospect Eden's Administrative Menu. Type 'help' for help.<br />";
+            const welcomeMessage = "PE-T1 Data Terminal Osiris.<br />Stardate 2552.<br />ROM BIOS LOADED v9.2 - rev 7<br />RAM CRC check: (000000h)<br />Drive - Serial: 4204694 - DETECTED<br />INPUT SYSTEM - VIRTUAL<br />Booting.<br />Ports SECURE: [TAPS: 00]<br />Encryption:[ENABLED-204umn1]<br />Virus Scan - '.....CHECKING…..'<br />BOOT: ProspectEdenAdmin.exe<br />BOOT SUCCESSFUL.<br />WELCOME.<br />Welcome USER to Prospect Eden's Administrative Menu. Type 'help' for help.<br />";
             let currentIndex = 0;
 
             function typeWelcomeMessage() {
@@ -23,13 +23,13 @@
                     // Handle different commands and display responses
                     switch (command) {
                         case "help":
-                            output.innerHTML += "Available commands:<br/>- help: Show available commands<br/>- date: Display current date<br/>- clear: Clear the screen<br/>";
+                            typeResponse("Available commands:<br/>- help: Show available commands<br/>- date: Display current date<br/>- clear: Clear the screen<br/>");
                             break;
 
                         case "date":
                             const currentDate = new Date();
                             const formattedDate = currentDate.toLocaleString();
-                            output.innerHTML += `Current date and time: ${formattedDate}<br/>`;
+                            typeResponse(`Current date and time: ${formattedDate}<br/>`);
                             break;
 
                         case "clear":
@@ -38,8 +38,23 @@
                             break;
 
                         default:
-                            output.innerHTML += `Command not recognized: ${command}<br/>`;
+                            typeResponse(`Command not recognized: ${command}<br/>`);
                     }
                 }
             });
+
+            // Function to simulate typing for responses
+            function typeResponse(response) {
+                let responseIndex = 0;
+
+                function typeNextCharacter() {
+                    if (responseIndex < response.length) {
+                        output.innerHTML += response.charAt(responseIndex);
+                        responseIndex++;
+                        setTimeout(typeNextCharacter, 50); // Adjust typing speed here
+                    }
+                }
+
+                typeNextCharacter(); // Start typing the response
+            }
         });
