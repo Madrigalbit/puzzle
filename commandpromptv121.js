e8189639ebf2143f8d482e7cdc4fc9a6b17dd536
Matthew Madrigal <46862219+Madrigalbit@users.noreply.github.com>
2023-11-03 00:51:09 -0700

    Update and rename commandpromptv120.js to commandpromptv121.js

--- commandpromptv121.js
+++ commandpromptv121.js
@@ -81,6 +81,7 @@ document.addEventListener('keydown', function(event) {
             clearTimeout(audioTimer); // Clear the previous timer
         }
         playAudio(); // Call the playAudio function on keydown
+        isKeyHeld = true; // Set the flag to indicate a key is held down
     }
 });
 
@@ -88,14 +89,7 @@ document.addEventListener('keyup', function(event) {
     if (event.key === 'Enter' || event.key === ' ') {
         isKeyHeld = false;
     }
-    if (audioTimer) {
-        clearTimeout(audioTimer); // Clear the timer when a key is released
-    }
-    audioTimer = setTimeout(function () {
-        audioTimer = null; // Reset the timer
-    }, 500); // Adjust the delay as needed
 });
-
 function handleCommand(command) {
     // Check if the entered command requires a password
     const requiredPassword = commandConfig[command.toLowerCase()];
